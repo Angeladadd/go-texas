1e583b2629ea5bf9e1486f556c4db0291f02407e
Angeladaddy <sjtusunchenge@gmail.com>
2025-02-10 11:43:18 +0000

    add explain tag

--- go_texas.py
+++ go_texas.py
@@ -8,14 +8,25 @@ import cnlunar
 from wuxing import texas_advice, texas_fortune
 
 
-happy_texas = '纳财'
+happy_texas =  ['纳财', '出师', '庆赐']
+sad_texas = ['施恩', '入学']
+
+def get_lunar_advice(goodThing):
+    return any([thing in goodThing for thing in happy_texas]) - any([thing in goodThing for thing in sad_texas]) >= 0
 
 def main():
 
+    parent_parser = argparse.ArgumentParser(add_help=False)
+    parent_parser.add_argument(
+        '-e', '--explain', 
+        dest='explain', 
+        action='store_true',
+        help='Explain the fortune'
+    )
     parser = argparse.ArgumentParser(description='Texas fortune for today')
     subparsers = parser.add_subparsers(dest='who')
-    me_parser = subparsers.add_parser('me', help='Test the fortune for yourself')
-    friend_parser = subparsers.add_parser('friend', help='Test the fortune for a friend')
+    me_parser = subparsers.add_parser('me', parents=[parent_parser], help='Test the fortune for yourself')
+    friend_parser = subparsers.add_parser('friend', parents=[parent_parser], help='Test the fortune for a friend')
     friend_parser.add_argument('name', help='Friend\'s name')
     # it should be optional after we can predict 五行 by name
     friend_parser.add_argument('birthday', help='Friend\'s birthday')
@@ -36,17 +47,18 @@ def main():
     
     today = datetime.now()
     lunar = cnlunar.Lunar(today, godType='8char')
-    lunar_advice = happy_texas in lunar.goodThing
+    lunar_advice = get_lunar_advice(lunar.goodThing)
     me_advice, friend_advice = False, False
+    elements = {}
 
     if args.who == 'me':
         user_birthday = datetime.strptime(config['me']['birthday'], '%d-%m-%Y')
-        suitability, _ = texas_fortune(user_birthday)
+        suitability, elements = texas_fortune(user_birthday)
         me_advice = lunar_advice and texas_advice(suitability)
     elif args.who == 'friend':
         # TODO: save friend's birthday to config
         friend_birthday = datetime.strptime(args.birthday, '%d-%m-%Y')
-        suitability, _ = texas_fortune(friend_birthday)
+        suitability, elements = texas_fortune(friend_birthday)
         friend_advice = lunar_advice and texas_advice(suitability)
     else:
         print('Command unsupported: ', args.who)
@@ -65,6 +77,10 @@ def main():
             print('Your friend has a lucky day. DO NOT GO TEXAS WITH THEM!')
         else:
             print('Your friend has a bad day. GO TEXAS WITH THEM!')
+    
+    if args.explain:
+        print('Good thing for today:', lunar.goodThing)
+        print('Texas fortune:', elements)
 
 
 if __name__ == '__main__':
