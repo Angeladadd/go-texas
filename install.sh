f83658a421d3a7188dd9cd10f0e82b04f2ae8fd5
Angeladaddy <sjtusunchenge@gmail.com>
2025-02-10 00:17:58 +0000

    fix apple silicon

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
-pip3 install -r requirements.txt
+if [[ "$(uname)" == "Darwin" && "$(uname -m)" == "arm64" ]] ; then
+    pip3 install --break-system-packages -r requirements.txt
+else
+    pip3 install -r requirements.txt
+fi
 python3 install_helper.py
 sudo cp gotexas /usr/local/bin
 sudo chmod +x /usr/local/bin/gotexas
